91ea91a7ee3a841ff3c3e9cc6febbed9ac1c1425
jrnn21 <57282294+jrnn21@users.noreply.github.com>
2021-01-14 08:13:01 +0700

    delete

--- src/screen/Dashboard.js
+++ src/screen/Dashboard.js
@@ -4,11 +4,7 @@ const Dashboard = () => {
  return (
   <>
    <h3>HOME PAGE</h3>
-   <h1>clone finish</h1>
-   <h1>clone again</h1>
-   <h4>ggggggggg</h4>
-   <h1>clone again</h1>
-   <small>gg</small>
+   
   </>
  );
 };
